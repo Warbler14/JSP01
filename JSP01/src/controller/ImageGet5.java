d6fc68f4cafedd4987002a51355dab0dfe662dfb
Warbler14 <firefly11a@naver.com>
2017-04-21 01:25:37 +0900

    multiple line draw test

---
+++ JSP01/src/controller/ImageGet5.java
@@ -0,0 +1,169 @@
+package controller;
+
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.imageio.ImageIO;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+/**
+ * http://localhost:8080/JSP01/drawSpiral?width=500&height=500&x1=250&y1=250&degree=10&radius=50&count=50
+ */
+@WebServlet("/drawSpiral")
+public class ImageGet5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	static Logger logger = Logger.getLogger(ImageGet5.class);
+
+	public final static String [] PARAMETERS = {"width", "height", "x1","y1","degree", "radius", "count"};
+	
+    public ImageGet5() {
+        super();
+        
+    }
+    
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		//---------------------------------------------------------------------------------
+		int dataArray [] = new int[PARAMETERS.length];
+		
+		ArrayList<String> messagelist = null;
+		boolean isParamOK = false;
+		//---------------------------------------------------------------------------------
+		
+		try{
+			int i = 0;
+
+			for( int j = PARAMETERS.length ; i< j ; i++ ){
+				String param = request.getParameter( PARAMETERS[i] );
+				
+				if( ( param == null ) || param.length() <= 0 ){
+					isParamOK = false;
+					break;
+					
+				}else{
+					
+					dataArray[i] = Integer.valueOf( param );
+				
+				}
+				
+				logger.debug( "[" + i + "] " + PARAMETERS[i] + " : " + dataArray[i] );
+			}//end for
+			logger.debug( "----------------------------------------------------------" );
+			
+			if( i == PARAMETERS.length ){
+				isParamOK = true;
+			}
+			
+		}catch(NumberFormatException n){
+			if(messagelist == null){
+				messagelist = new ArrayList<String>();
+			}
+			
+			messagelist.add( n.getMessage() );
+			
+			logger.error("NumberFormatException : " + n.getMessage() );
+			
+		}catch (Exception e) {
+			if(messagelist == null){
+				messagelist = new ArrayList<String>();
+			}
+			
+			messagelist.add( e.getMessage() );
+			
+			logger.error("Exception : " + e.getMessage() );
+		}
+		
+		
+		//---------------------------------------------------------------------------------
+		
+		if( isParamOK ){
+			
+			
+			
+			
+			try{
+				
+				paintProcessJPG( response, BufferedImage.TYPE_INT_RGB , Color.white , dataArray);
+				
+			}catch(Exception e){
+				logger.error("Exception : " + e.getMessage() );
+			}
+			
+			
+			
+			
+		}else{
+			response.getWriter().println("<h1> bad prameter </h1>");
+			if((messagelist != null) && !messagelist.isEmpty() ){ 
+				for(int i = 0 , j = messagelist.size() ; i<j ; i++ ){
+					response.getWriter().println("<h1> "+ messagelist.get(i) +" </h1>");
+				}
+			}
+		}
+		
+	}// end doGet
+	
+	
+	private static void paintProcessJPG( HttpServletResponse response
+									, int imageType, Paint baseColor
+									, int dataArray []) throws Exception{
+		
+		//----------------------------------------------
+		response.setContentType("image/jpeg");
+		BufferedImage bi = new BufferedImage(dataArray[0], dataArray[1], imageType);
+		Graphics2D g = (Graphics2D) bi.getGraphics();
+		g.setPaint(baseColor);
+		//----------------------------------------------
+		
+		//"width(0)", "height(1)", "x1(2)", "y1(3)", "degree(4)", "radius(5)", "count(6)"
+		
+		/*
+		for(int i = 0 ; i < dataArray[6] ; i++ ){
+			
+			//get position
+			int positions [] = getPoint( dataArray[2], dataArray[3], dataArray[4] , dataArray[5] );
+			
+			//draw Line
+			g.drawLine(dataArray[2],dataArray[3], positions[0],  positions[1] );
+			
+			//reposition
+			dataArray[2] = positions[0];
+			dataArray[3] = positions[1];
+		}
+		*/
+		/*
+		Line2D line1 = new Line2D.Double(10.0, 10.0, 20.0, 20.0);
+		Line2D line2 = new Line2D.Double(20.0, 20.0, 40.0, 10.0);
+		Line2D line3 = new Line2D.Double(40.0, 10.0, 60.0, 20.0);
+		g.draw(line1);
+		g.draw(line2);
+		g.draw(line3);
+		*/
+		g.drawLine(10, 10, 20, 20);
+		g.drawLine(20 , 20, 40, 10);
+		g.drawLine(20 , 20, 40, 90);
+		
+		//----------------------------------------------
+		OutputStream out = response.getOutputStream();
+		ImageIO.write(bi, "jpg", out);
+		out.close();
+		//----------------------------------------------
+		
+	}
+	
+}
